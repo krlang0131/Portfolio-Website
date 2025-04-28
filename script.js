1d8a952abdf20c09f9b935f85296490191ff45a1
krlang0131 <krlang0131@gmail.com>
2025-04-28 16:56:55 -0400

    responsive

---
+++ script.js
@@ -0,0 +1,12 @@
+// Open the side menu
+function openMenu() {
+    document.getElementById("sideMenu").style.width = "250px";
+    document.getElementById("overlay").style.display = "block";
+  }
+  
+  // Close the side menu
+  function closeMenu() {
+    document.getElementById("sideMenu").style.width = "0";
+    document.getElementById("overlay").style.display = "none";
+  }
+  
